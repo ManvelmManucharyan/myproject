b360e057cea6975c3a841d2e1e6cce4abc12d0d6
ManvelmManucharyan <manvelmanucharyan98@gmail.com>
2022-06-15 23:30:08 +0400

    working after route

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,7 @@ const UserServices = require('../services/UserServices')
 
 class UserController {
     static async getAllUsers (req, res){
-        const users = await UserServices;
+        const users = await UserServices.getAllUsers();
         res.status(200).send(users)
     }
 }
--- index.js
+++ index.js
@@ -1,55 +1,23 @@
 const sequelize = require('./db/connection');
 const bodyParser = require('body-parser')
 const express = require('express');
-const employee = require('./models/employee');
-const position = require('./models/position')
+const routes = require('./routes/index');
+const employee = require('./models/employee')
 
 const app = express();
 const PORT = 3000
-const PORT1 = 3001
 
 app.use(bodyParser.json())
+app.use('/api/v1/', routes);
 
-const startEmployee = async ()=>{
+const start = async ()=>{
     try{
         app.listen(PORT)
         await sequelize.authenticate();
-        app.post('/', async (req, res)=>{
-            await employee.create({
-                name: req.body.name,
-                age: req.body.age,
-                email: req.body.email
-            })
-            res.status(201).send('Successfully created');
-        })
-        app.get('/', async (req, res)=>{
-            const users = (await employee.findAll()).map(el=> el.getDataValue('name'));
-            res.send(users)
-        })
         console.log('Database successfully connected')
     }catch (error) {
         console.log('Unable connect to database')
     }
 }
 
-// startEmployee();
-
-const startPosition = async ()=>{
-    try{
-        app.listen(PORT1)
-        await sequelize.authenticate();
-        app.post('/', async (req, res)=>{
-            await position.create({
-                position: req.body.position,
-                salary: req.body.salary
-            })
-            console.log(req.body)
-            res.status(201).send('Successfully created');
-        })
-        console.log('Database successfully connected')
-    }catch (error) {
-        console.log('Unable connect to database')
-    }
-}
-
-startPosition();
+start();
